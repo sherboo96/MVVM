e53123a53b11fe2e729ea12ea374b5e2a9a3f8c3
sherbeny96 <sherbeny2096@gmail.com>
2021-09-26 19:29:00 +0200

    Initial Commit

---
+++ MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM
+//
+//  Created by Mahmoud Sherbeny on 26/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
