7288a094c7407ecf587cacf435a5703802c0f364
sherbeny96 <sherbeny2096@gmail.com>
2021-09-30 20:16:00 +0200

    Update

--- MVVM/Scenes/More/MoreVC.swift
+++ MVVM/Scenes/More/MoreVC.swift
@@ -14,7 +14,6 @@ class MoreVC: BaseWireFrame<MoreViewModel, MoreVCRouterProtocol> {
         router.presentEmptyScreen()
     }
 
-
     override func bind(viewModel: MoreViewModel) {
         
     }
